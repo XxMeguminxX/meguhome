387e0ddd396c824949e23865dba9e69290c15848
XxMeguminxX <saputraerik042@gmail.com>
2026-06-25 21:16:36 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,20 @@ set -e
 
 REMOTE_USER="${DEPLOY_USER:-root}"
 REMOTE_HOST="${DEPLOY_HOST:?Error: DEPLOY_HOST is not set}"
-REMOTE_PATH="/var/www/mihome"
+REMOTE_PATH="/var/www/meguhome"
+NGINX_CONF_SRC="./nginx.conf"
+NGINX_CONF_DEST="/etc/nginx/sites-available/meguim.com"
 
 echo "Building..."
 npm run build
 
-echo "Uploading to $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
+echo "Uploading dist to $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
 rsync -avz --delete dist/ "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH/"
+
+echo "Uploading nginx config..."
+scp "$NGINX_CONF_SRC" "$REMOTE_USER@$REMOTE_HOST:$NGINX_CONF_DEST"
+
+echo "Reloading nginx..."
+ssh "$REMOTE_USER@$REMOTE_HOST" "nginx -t && systemctl reload nginx"
 
 echo "Deploy complete."
